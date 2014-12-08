f1eddc7b95cdcf368e5584f380759ef24f4694dd
Huan Gao <hg2357@columbia.edu>
2014-12-07 22:37:38 -0800

    NlpService done

--- src/main/java/com/e6893/education/erp/api/TestController.java
+++ src/main/java/com/e6893/education/erp/api/TestController.java
@@ -6,10 +6,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.e6893.education.erp.services.NlpService;
 
 @Controller
 public class TestController {
@@ -17,22 +21,15 @@ public class TestController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
 
+	@Autowired
+	NlpService nlpService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-				DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
+	@RequestMapping(value = "/nlp/{searchSentence}", method = RequestMethod.GET)
+	public String test(@PathVariable String searchSentence) {
+		System.out.println(nlpService.getTopics(searchSentence));
 		
-
 		return "showMessage";
 	}
 
--- src/main/java/com/e6893/education/erp/services/NlpService.java
+++ src/main/java/com/e6893/education/erp/services/NlpService.java
@@ -1,10 +1,19 @@
 package com.e6893.education.erp.services;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.parser.Parse;
@@ -12,16 +21,19 @@ import opennlp.tools.parser.Parser;
 import opennlp.tools.parser.ParserFactory;
 import opennlp.tools.parser.ParserModel;
 
-
+@Service
 public class NlpService {
 	
 	static Set<String> nounPhrases = new HashSet<>();
 	
-	public static Set<String> getTopics(String searchSentence) {
+	@Autowired
+	ServletContext servletContext;
+	
+	public Set<String> getTopics(String searchSentence) {
 		InputStream modelInParse = null;
 		try {
-			//load chunking model
-			modelInParse = new FileInputStream("/config/en-parser-chunking.bin"); //from http://opennlp.sourceforge.net/models-1.5/
+			File modelDir = new File( servletContext.getRealPath("/WEB-INF/config/en-parser-chunking.bin") );
+			modelInParse = new FileInputStream(modelDir); //from http://opennlp.sourceforge.net/models-1.5/
 			ParserModel model = new ParserModel(modelInParse);
 			
 			//create parse tree
@@ -31,6 +43,10 @@ public class NlpService {
 			//call subroutine to extract noun phrases
 			for (Parse p : topParses)
 				getNounPhrases(p);
+
+			if (nounPhrases.isEmpty())
+				nounPhrases.add(searchSentence);
+			
 			return nounPhrases;
 			
 		}
@@ -53,11 +69,20 @@ public class NlpService {
 	
 	//recursively loop through tree, extracting noun phrases
 	public static void getNounPhrases(Parse p) {
-			
-	    if (p.getType().equals("NP")) { //NP=noun phrase
+	    if (p.getType().equals("NP") || p.getType().equals("NN") || p.getType().equals("NNS")) { //NP=noun phrase
 	         nounPhrases.add(p.getCoveredText());
 	    }
 	    for (Parse child : p.getChildren())
 	         getNounPhrases(child);
 	}
+
+	
+
+//	@Override
+//	public void setResourceLoader(ResourceLoader arg0) {
+//		// TODO Auto-generated method stub
+//	    this.resourceLoader = resourceLoader;
+//	}
+
+	
 }
